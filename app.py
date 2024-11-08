68426711b62f60e3682dbd8ea2aa6db19d2cb210
alvaro.gonzalez <alvaro.gonzalez@csc.fi>
2024-11-08 14:51:04 +0200

    Revert "NO need for that"
    This reverts commit bd964d05694301725618c82162bbcb2dee0aff7a.

--- app.py
+++ app.py
@@ -175,6 +175,8 @@ def listenBuild(secret):
   if not secret == config["secret"]:
     return "Access denied"
 
+  build()
+
   response = Response('built started')
 
   response.call_on_close(build)
